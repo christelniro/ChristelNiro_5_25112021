68078b40fe51529708493f28f231b6024e6e6199
christelnsewosolo <84200905+christelnsewosolo@users.noreply.github.com>
2021-12-08 20:24:57 +0100

    creation api

--- back/server.js
+++ back/server.js
@@ -45,10 +45,3 @@ server.on('listening', () => {
 
 server.listen(port);
 
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
-});
-
-server.listen(process.env.PORT || 3000);
