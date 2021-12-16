bac5a2ee136f5319b91f03994d5495f44c623071
christelnsewosolo <84200905+christelnsewosolo@users.noreply.github.com>
2021-12-16 11:12:53 +0100

    récupérer les détails du produit

--- front/js/product.js
+++ front/js/product.js
@@ -3,3 +3,15 @@ let str = window.location.href;
 let url = new URL(str);
 let idProduct = url.searchParams.get("id");
 console.log(idProduct)
+
+//récupérer les détails du produit
+
+    const recuperationProduit = async () => {
+        await fetch(`http://localhost:3000/api/products/${idProduct}`)
+        .then((res) => res.json())
+        .then((promise) => {
+            console.log(promise);
+        });
+    }
+    recuperationProduit();
+    
