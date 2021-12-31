c9bee57c8e635b7c668d4bacd7489dc3216739b7
christelnsewosolo <84200905+christelnsewosolo@users.noreply.github.com>
2021-12-31 01:48:46 +0100

    supression article ds panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,7 +4,7 @@
  //JSON sert a convertir les donnéees au format jspn qui sont ds le local storage
 console.log(produitEnrgDansLeLocaleStorage);
 
-//afffcihage pdt ds le panier
+//------------------------------------afffcihage pdt du panier
 
 //selection de la classe ou je vais injincter le code html
 
@@ -13,39 +13,71 @@ console.log(positionElement1)
 
 //si le panier est vide afficher panier vide
 
-if(produitEnrgDansLeLocaleStorage === null){
+if(produitEnrgDansLeLocaleStorage === null || produitEnrgDansLeLocaleStorage == 0){
 const panierVide = `<p> Votre panier est vide </p>`;
 positionElement1.innerHTML = panierVide;
 } else { 
     //si panier pas vide mettre les article en local storage
-    //insertion de l'élément article
+    
     let structureProduitPanier = [];
 
     for(k = 0; k < produitEnrgDansLeLocaleStorage.length; k++ ){
         
         structureProduitPanier = structureProduitPanier + `
         
-                <div class="cart__item__img"${produitEnrgDansLeLocaleStorage[k].imgProduit}>
+                <div class="cart__item__img">
+<img src=${produitEnrgDansLeLocaleStorage[k].imgProduit} alt=${produitEnrgDansLeLocaleStorage[k].imgProduit.altImgProduit}>
+                </div>
                 </div>
                 <div class="cart__item__content">
                   <div class="cart__item__content__description">
                     <h2>Nom du produit ${produitEnrgDansLeLocaleStorage[k].nomProduit}</h2>
-                    <p>Vert ${produitEnrgDansLeLocaleStorage[k].choixCouleur}</p>
+                    <p>${produitEnrgDansLeLocaleStorage[k].couleurProduit}</p>
                     <p> € ${produitEnrgDansLeLocaleStorage[k].prixProduit}</p>
                   </div>
                   <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : ${produitEnrgDansLeLocaleStorage[k].quantiteProduit}</p>
+                  <p>Qté : ${produitEnrgDansLeLocaleStorage[k].quantiteProduit}</p>
                       
                     </div>
                     <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
+                      <button class="deleteItem"> Supprimer </button>
                     </div>
                   </div>
                 </div>
         `;}
-
-        if(k=== produitEnrgDansLeLocaleStorage.length){
+        if (k == produitEnrgDansLeLocaleStorage.length){
+            //injection html dans la page panier
         positionElement1.innerHTML = structureProduitPanier;
     }
+    }
+
+    //fin affichage pdt du panier------------------------------------------
+
+    //-----gestion btn panier---
+    //selection des ref du btn supprime
+
+    let btn_supprimer = document.querySelectorAll(".deleteItem")
+    console.log(btn_supprimer);
+
+
+    for (let l = 0; l < btn_supprimer.length; l++){
+        btn_supprimer[l].addEventListener("click" , (event) =>{
+            event.preventDefault();
+
+            let id_selectionner_supression = produitEnrgDansLeLocaleStorage[l].idProduit;
+            console.log("id_selectionner_supression");
+            console.log(id_selectionner_supression);
+
+produitEnrgDansLeLocaleStorage = produitEnrgDansLeLocaleStorage.filter(
+    el => el.idProduit !== id_selectionner_supression);
+console.log(produitEnrgDansLeLocaleStorage);
+
+localStorage.setItem("produit", JSON.stringify(produitEnrgDansLeLocaleStorage));
+
+//Alerte produit supprimé et refresh
+alert("Ce produit a bien été supprimé du panier");
+location.reload();
+
+
+        })
     }
