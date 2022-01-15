bf33c309c8fe61fb672093998801b79c47ea498a
christelnsewosolo <84200905+christelnsewosolo@users.noreply.github.com>
2022-01-15 15:12:59 +0100

    test betty

--- front/js/cart.js
+++ front/js/cart.js
@@ -96,11 +96,12 @@ add.addEventListener('click', function(){
 integer += 1;
 int.innerHTML = integer;
 })
+
 remove.addEventListener('click', function(){
     integer -= 1;
     int.innerHTML = integer;
     })
-
+console.log(add);
 //-----------------------------------------------------------------------------------
 
      //----------------------- Récupération du total des quantités----------------------
